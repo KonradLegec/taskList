e8530526728f02a40c2764713d8a75c5176c46a7
Konrad Legec <konrad.legec@gmail.com>
2022-09-10 13:12:59 +0200

    added comments to script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 {
-
+  //"tasks" to tablica przechowująca zadania oraz ich status "done" z wartościami boolean(true/false)
   const tasks = [
     {
       content: "nagrać lekcję",
@@ -10,7 +10,7 @@
       done: true,
     },
   ];
-
+  // funkcja "render" generuje zawartość listy ul po modyfikacji jej zawartości
   const render = () => {
     let htmlString = "";
 
@@ -33,29 +33,31 @@
     });
   };
 
+  // "addNewTask" dodaje do listy zadań treść nowego zadania przekazanego w referencji w funkcji "onFormSubmit" i wywołuje funkcję "render()"
   const addNewTask = (newTaskContent) => {
     tasks.push({
       content: newTaskContent,
     });
     render();
   };
-
+  
+  // "removeTask" usuwa z listy zadań zadanie o wybranym indeksie i wywołuje funkcję render()
   const removeTask = (taskIndex) => {
     tasks.splice(taskIndex, 1);
     render();
   };
 
+  // "onFormSubmit" blokuje wysłanie formularza, pobiera z inputa wartość nowego zadania i pozbawia ją białych znaków oraz sprawdza czy nie jest pusta. Następnie zostaje wywołana funkcja "addNewTask" ze stałą "newTaskContent"
   const onFormSubmit = (e) => {
     e.preventDefault();
-
     const newTaskContent = document.querySelector(".js-newTask").value.trim();
-
     if (newTaskContent === "") {
       return;
     }
     addNewTask(newTaskContent);
   };
 
+  // "init" nasłuchuje submit na formularzu następnie wywołuje funkcję "onFormSubmit"
   const init = () => {
     render();
     const form = document.querySelector(".js-form");
