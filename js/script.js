4316e8a46580697906e15c7ec1b021a7da352c6d
Konrad Legec <konrad.legec@gmail.com>
2022-09-10 12:51:56 +0200

    initial commit

---
+++ js/script.js
@@ -0,0 +1,67 @@
+{
+
+  const tasks = [
+    {
+      content: "nagrać lekcję",
+      done: false,
+    },
+    {
+      content: "zjeść pierogi",
+      done: true,
+    },
+  ];
+
+  const render = () => {
+    let htmlString = "";
+
+    for (const task of tasks) {
+      htmlString += `
+        <li ${task.done ? 'style ="text-decoration: line-through"' : ""}>
+            ${task.content}<button class= "js-remove">Usuń</button>
+        </li>
+        `;
+    }
+
+    document.querySelector(".js-tasks").innerHTML = htmlString;
+    const removeButtons = document.querySelectorAll(".js-remove");
+
+    removeButtons.forEach((removeButton, taskIndex) => {
+      console.log(removeButtons);
+      removeButton.addEventListener("click", () => {
+        removeTask(taskIndex);
+      });
+    });
+  };
+
+  const addNewTask = (newTaskContent) => {
+    tasks.push({
+      content: newTaskContent,
+    });
+    render();
+  };
+
+  const removeTask = (taskIndex) => {
+    tasks.splice(taskIndex, 1);
+    render();
+  };
+
+  const onFormSubmit = (e) => {
+    e.preventDefault();
+
+    const newTaskContent = document.querySelector(".js-newTask").value.trim();
+
+    if (newTaskContent === "") {
+      return;
+    }
+    addNewTask(newTaskContent);
+  };
+
+  const init = () => {
+    render();
+    const form = document.querySelector(".js-form");
+
+    form.addEventListener("submit", onFormSubmit);
+  };
+
+  init();
+}
