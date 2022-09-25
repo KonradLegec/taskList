4d46a1e471dec00225c60e927cade16594a06938
Konrad Legec <konrad.legec@gmail.com>
2022-09-25 21:57:59 +0200

    implemented immutability and management buttons

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 {
-  const tasks = [];
+  let tasks = [];
+  let hideShowTasks = false;
 
   const bindEvents = () => {
     const removeButtons = document.querySelectorAll(".js-remove");
@@ -19,11 +20,39 @@
     });
   };
 
+  const bindButtonsEvents = () => {
+    const switchDoneTasks = document.querySelector(".js-switchDoneTasks");
+    const markAllTasksDone = document.querySelector(".js-markAllTasksDone");
+
+    switchDoneTasks?.addEventListener("click", switchTasksDone);
+    markAllTasksDone?.addEventListener("click", markDoneAllTasks);
+  };
+
+  const switchTasksDone = () => {
+    hideShowTasks = !hideShowTasks;
+    render();
+  };
+
+  const markDoneAllTasks = () => {
+    tasks = tasks.map((task) => ({
+      ...task,
+      done: true,
+    }));
+    render();
+  };
+
   const render = () => {
+    renderTasks();
+    renderButtons();
+    bindEvents();
+    bindButtonsEvents();
+  };
+
+  const renderTasks = () => {
     let htmlString = "";
     for (const task of tasks) {
       htmlString += `
-        <li class = "taskList__item">
+        <li class="${task.done && hideShowTasks ? " taskList__item--hidden" : "taskList__item"}">
             <button class = "js-done taskList__toggleButton">
             ${task.done ? "✔" : ""}
             </button>
@@ -36,7 +65,23 @@
         `;
     }
     document.querySelector(".js-tasks").innerHTML = htmlString;
-    bindEvents();
+  };
+
+  const renderButtons = () => {
+    const managementButtons = document.querySelector(".js-managementButtons");
+    if (tasks.length === 0) {
+      managementButtons.innerHTML = "";
+      return;
+    }
+    managementButtons.innerHTML = `
+        <button class="taskList__button js-switchDoneTasks">
+            ${hideShowTasks ? "Pokaż" : "Ukryj"} ukończone
+        </button>
+        <button class="taskList__button js-markAllTasksDone"
+        ${tasks.every(({ done }) => done) ? "disabled" : ""}>
+        Ukończ wszystkie
+        </button>
+        `;
   };
 
   const onFormSubmit = (e) => {
@@ -46,9 +91,7 @@
     if (newTaskContent === "") {
       newTaskInput.focus();
       return;
-    }
-
-    else if (newTaskContent !== "") {
+    } else if (newTaskContent !== "") {
       addNewTask(newTaskContent);
       newTaskInput.value = "";
       newTaskInput.focus();
@@ -65,19 +108,21 @@
   init();
 
   const addNewTask = (newTaskContent) => {
-    tasks.push({
-      content: newTaskContent,
-    });
+    tasks = [...tasks, { content: newTaskContent }];
     render();
   };
 
   const removeTask = (taskIndex) => {
-    tasks.splice(taskIndex, 1);
+    tasks = [...tasks.slice(0, taskIndex), ...tasks.slice(taskIndex + 1)];
     render();
   };
 
   const toggleTaskDone = (taskIndex) => {
-    tasks[taskIndex].done = !tasks[taskIndex].done;
+    tasks = [
+      ...tasks.slice(0, taskIndex),
+      { ...tasks[taskIndex], done: !tasks[taskIndex].done },
+      ...tasks.slice(taskIndex + 1),
+    ];
     render();
   };
 }
